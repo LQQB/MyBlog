1351ed0224eafefeb349f85cb2a4418fec06a7d0
LQB <897680736@qq.com>
2017-07-18 17:49:54 +0800

    使用工厂模式 生成运用对象 APP

--- LQBBlog/__init__.py
+++ LQBBlog/__init__.py
@@ -1,22 +1,23 @@
 from flask import Flask,redirect, url_for
 
 from LQBBlog.config import DevConfig
-from LQBBlog.controllers import blog
+from LQBBlog.controllers import blog, main
 from LQBBlog.models import db
-app = Flask(__name__)
+from LQBBlog.extensions import bcrypt
 
-# 使用 onfig.from_object() 而不使用 app.config['DEBUG'] 是因为这样可以加载
-# class DevConfig 的配置变量集合，而不需要一项一项的添加和修改。
-app.config.from_object(DevConfig)
+def create_app(object_name):
 
-db.init_app(app)
+    app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return redirect(url_for('blog.home'))
+    # 使用 onfig.from_object() 而不使用 app.config['DEBUG'] 是因为这样可以加载
+    # class DevConfig 的配置变量集合，而不需要一项一项的添加和修改。
+    app.config.from_object(object_name)
 
-app.register_blueprint(blog.blog_blueprint)
+    db.init_app(app)
+    
+    bcrypt.init_app(app)
 
-if __name__ == '__main__':
+    app.register_blueprint(blog.blog_blueprint)
+    app.register_blueprint(main.main_blueprint)
 
-    app.run()
+    return app
--- manage.py
+++ manage.py
@@ -1,26 +1,34 @@
+import os
+
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Server  # 最新2.0.5版本改了语法
 
-from LQBBlog import models, __init__
+from LQBBlog import models, create_app
 
-manager = Manager(__init__.app)
 
-migrate = Migrate(__init__.app, models.SQLAlchemy())
+env = os.environ.get('flakWeb', 'dev')
+print('LQBBlog.config.%sConfig' %env.capitalize())
+app = create_app('LQBBlog.config.%sConfig' %env.capitalize())
 
-manager.add_command('server', Server() )
+
+manager = Manager(app)
+
+migrate = Migrate(app, models.db)
+
+manager.add_command('server', Server(host='192.168.58.1', port=5000) )
 manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
     # 我们 每在 models.py 中新定义一个数据模型, 都需要在 manager.py 中导入并添加到返回 dict 中.
-    return dict(app=__init__.app,
+    return dict(app=app,
                 db=models.db,
                 User=models.User,
                 Post=models.Post,
                 Comment=models.Comment,
-                Tag=models.Tag)
+                Tag=models.Tag,
+                Server=Server)
 
-# 通过 manager.py 来执行命令行是十分有必要的，因为一些 Flask 的扩展只有在 Flask app object 被创建之后才会被初始化，
-# 所以非常依赖于应用上下文的环境，在没有 Flask app object 时，直接运行默认的 python CLI 会导致这些 Flask 扩展返回错误。
+# 通过 manager.py 来执行命令行是十分有必要lask app object 时，直接运行默认的 python CLI 会导致这些 Flask 扩展返回错误。
 if __name__ == '__main__':
     manager.run()
