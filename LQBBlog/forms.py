969d7c9a72dda9d8a4ce033e65221e049e4c61d9
LQB <897680736@qq.com>
2017-07-18 17:53:22 +0800

    Flask Bcrypt 模块引入 (使用 Bcrypt 哈希算法 对重要信息进行加密)

--- LQBBlog/forms.py
+++ LQBBlog/forms.py
@@ -1,10 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
-from wtforms.validators import  DataRequired, Length
+from wtforms import StringField, TextAreaField, PasswordField
+from wtforms.validators import  DataRequired, Length, EqualTo
+from LQBBlog.models import db, User
 
 
 class CommentForm(FlaskForm):
-
+    '''
+    评论 校验
+    '''
     name = StringField(
         'Name',
         validators = [DataRequired(), Length(max=255)]  )
@@ -14,3 +17,46 @@ class CommentForm(FlaskForm):
         description = u'请提出宝贵意见和建议',
         validators = [DataRequired()],
         render_kw= {"placeholder": "评论。。。"}  )
+
+class LoginForm(FlaskForm):
+
+    username = StringField('UserName', [DataRequired(), Length(max=255)])
+    password = PasswordField('PassWord', [DataRequired()] )
+
+    def validator(self):
+        check_validator = super(LoginForm, self).validate()
+
+        if not check_validator:
+            return  False
+
+        user = User.query.filter_by(username=self.username.data).first()
+        if not user:
+            self.username.errors.append('有户名或密码无效')
+            return  False
+
+        if not user.check_password(self.password.data):
+            self.username.errors.append('有户名或密码无效')
+            return False
+
+class RegisterForm(FlaskForm):
+
+    username = StringField('UserName', [DataRequired(), Length(max=255)])
+    password = PasswordField('PassWord', [DataRequired(), Length(min=8)])
+    agin_password = PasswordField('Agin_PassWord', [DataRequired(), EqualTo('password')])
+
+    def validator(self):
+        check_validator = super(RegisterForm, self).validate()
+
+        if not check_validator:
+            return  False
+
+        user = User.query.filter_by(username=self.username.data).first()
+        if user:
+            self.username.errors.append('有户名或密码无效')
+            return False
+        return True
+
+class PostForm(FlaskForm):
+
+    title = StringField('Title', [DataRequired(), Length(max=255)])
+    text = TextAreaField('Blog Content', [DataRequired()])
